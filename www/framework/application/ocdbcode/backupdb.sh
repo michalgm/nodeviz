7426432ffa496640c0ddcb968fea7a6650a4b964
unfluence <unfluence@8a2d70d0-5736-8764-f290-76e388cdf711>
2011-09-28 23:07:58 +0000

    removed unused oilchange utility directories

--- www/framework/application/ocdbcode/backupdb.sh
+++
@@ -1,2 +0,0 @@
-#!/bin/sh
-mysqldump -u oilchange -poilchange oilchange > /home/media/backup2/`date +'%s'`.sql
--- www/framework/application/ocdbcode/update_data.sh
+++
@@ -1,3 +0,0 @@
-php import_ca_contribs.php
-php import_prop23_fec_data.php
-php prepare_data.php
